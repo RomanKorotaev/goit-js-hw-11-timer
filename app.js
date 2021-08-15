dcf67fa77b4ab4bd86299713f90192bc787fb631
Roman Korotaev <kro1986@ukr.net>
2021-08-15 19:23:39 +0300

    Задание с Таймером выполнено. ДОБАВИТЬ классы

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ function testTimerStart() {
   console.log('Страница загрузилась. Начался обратный отсчёт таймера!')  
 }
 
+const refs = {
+    days: document.querySelector('[data-value="days"]'),
+    hours: document.querySelector('[data-value="hours"]'),
+    mins: document.querySelector('[data-value="mins"]'),
+    secs: document.querySelector('[data-value="secs"]')
+}
+
+//start: document.querySelector('[data-action="start"]'),
 
 
 let startDate = Date.now();// Получаем точное время запуска таймера (момент времени , когда загрузилась наша страница)
@@ -20,18 +28,45 @@ let idTimer = null; //идентификатор таймера обратног
 
 //Это функция, которая будет выполнятся постоянно, пока тикает интервал (таймер обратного отсчёта). 
 //Передаём её в  setInterval
-function logger() {
-    console.log("run")
+function culcTime() {
+    const currentDate = Date.now();
+    const delta = targetDate - currentDate;
+    console.log("delta = ", delta);
+
+    //Путём деления получившихся милисекунд вычисляем секунды, минуты, часы, дни
+    const days = Math.floor(delta / (1000 * 60 * 60 * 24)); 
+    const hours = Math.floor((delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((delta % (1000 * 60)) / 1000);
+
+    console.log(`days: ${days}, hours: ${hours}, mins: ${mins}, secs: ${secs}`);
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.mins.textContent = mins;
+    refs.secs.textContent = secs;
 }
 
 // Это функция-обвёртка, которая передаётся в обработчик и запускается по событию, например клику кнопки.
 //Если в слушатель события сразу поставить вызов setInterval, то он запустится сразу зез клика. Но в данной заддаче нас это устраивает
 function timerStart() {
-    idTimer = setInterval(logger, 1000);
+    idTimer = setInterval(culcTime, 1000);
  }
 
-//idTimer = setInterval(logger, 1000);
 
 window.addEventListener("DOMContentLoaded", timerStart);
+
+
+ console.log('targetDate - startDate  = ', targetDate - startDate  )
+
+
+ /*
+window.addEventListener("click", stopTimer);
+ 
+function stopTimer() {
+    console.log("Таймер остановлен");
+    clearInterval(idTimer);
+}
+*/
+
 
 
